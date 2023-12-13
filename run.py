947500ccb20ccd480a69215d4637d415de2907b7
EmiCon97 <derleuchtturm97@gmail.com>
2023-12-13 13:15:36 -0300

    feature: create and populate collections. add id and vector id script

--- run.py
+++ run.py
@@ -3,25 +3,26 @@ from dotenv import load_dotenv
 import os
 from utils.embedding import Embedding
 from utils.read_files import question_data
+from questions import questions
+
+# def main():
+#     load_dotenv()
+#     openai.api_key = os.environ.get("OPENAI_API_KEY")
 
 
-def main():
+def embed_and_write(input):
     load_dotenv()
-    openai.api_key = os.environ.get("OPENAI_API_KEY")
+    embedding = Embedding(
+        os.environ.get("OPENAI_API_KEY"), os.environ.get("EMBEDDING_MODEL")
+    )
+    question_collection = embedding.create_collections("questions")
+    embedding.populate_collection(question_collection, input)
+    # questions = embedding.add_questions(input)
+    # embedding.write(questions)
 
 
-def embed(input):
-    load_dotenv()
-    embedding = Embedding(os.environ.get("OPENAI_API_KEY"))
-    return embedding.add_questions(input)
+# main()
+# embed_and_write("embed_example")
+# embed_testing(embedded_example)
 
-
-questions = [
-    "¿Cuál es el costo de asegurar mi envío?",
-    "¿Cómo puedo verificar mi envío?",
-    "¿Cómo puedo hacer el seguimiento de mi envío?",
-]
-
-main()
-# print(embed(questions))
-# question_data()
+embed_and_write(question_data())
--- utils/embedding.py
+++ utils/embedding.py
@@ -1,23 +1,58 @@
 import openai
+import chromadb
+from chromadb.utils.embedding_functions import OpenAIEmbeddingFunction
+import pandas as pd
+
+# import xlsxwriter
 
 
 class Embedding:
-    def __init__(self, api_key) -> None:
+    def __init__(self, api_key, embedding_model) -> None:
         openai.api_key = api_key
+        self.api_key = api_key
+        self.embedding_model = embedding_model
+        self.chroma_client = chromadb.EphemeralClient()
+
+    def create_collections(self, name):
+        embedding_function = OpenAIEmbeddingFunction(
+            api_key=self.api_key, model_name=self.embedding_model
+        )
+
+        return self.chroma_client.create_collection(
+            name=name, embedding_function=embedding_function
+        )
 
     @staticmethod
-    def __embed(input):
+    def populate_collection(collection, question_df):
+        collection.add(
+            ids=question_df.vector_id.values.tolist(),
+            embeddings=question_df.embed.values.tolist(),
+        )
+
+    def __embed(self, input):
         return (
             openai.embeddings.create(
-                model="text-embedding-ada-002", input=input, encoding_format="float"
+                model=self.embedding_model, input=input, encoding_format="float"
             )
             .data[0]
             .embedding
         )
 
     def add_questions(self, questions: list[str]):
-        embedded_questions = []
         for q in questions:
-            embedded_question = self.__embed(q)
-            embedded_questions.append({"embed": embedded_question, "question": q})
-        return embedded_questions
+            embedded_question = self.__embed(q["question"])
+            q["embed"] = embedded_question
+        return questions
+
+    @staticmethod
+    def __add_id_and_vector_id(questions):
+        for index, q in enumerate(questions):
+            q["id"] = index + 1
+            q["vector_id"] = index
+        return questions
+
+    def write(self, questions):
+        formatted_questions = self.__add_id_and_vector_id(questions)
+
+        df = pd.DataFrame(formatted_questions)
+        df.to_csv("files/vectors.csv", encoding="latin1", index=False)
