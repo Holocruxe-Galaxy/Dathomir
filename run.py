5be791aa11835e887833938cc7d4aa764a887f22
EmiCon97 <derleuchtturm97@gmail.com>
2023-12-19 21:52:09 -0300

    refactor: removed test.py

--- run.py
+++ run.py
@@ -72,4 +72,4 @@ def answerQuestion():
 CORS(app, origins="*")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, threaded=True, debug=True)
+    app.run(host="0.0.0.0", port=8080, threaded=True)
--- test.py
+++
@@ -1,8 +0,0 @@
-hola = "holalala"
-
-
-def ay():
-    print(hola[1:-1])
-
-
-ay()
